936a8d41f5f54226b5f6d8f596cc473f6d4ab477
tpslr <topias.lyyra@gmail.com>
2023-11-19 19:52:43 +0200

    frontend: added ability to upload videos

--- static/index.js
+++ static/index.js
@@ -25,6 +25,93 @@ function getRelativeDate(/** @type {number} */ timestamp) {
     return "in less than an hour";
 }
 
+function showError(message) {
+    alert(message);
+}
+
+
+class Upload {
+    /**@type {FormData} @readonly*/
+    formData;
+    /**@type {XMLHttpRequest} @readonly*/
+    xhr;
+    /**@type {HTMLDivElement} @readonly*/
+    videoDiv;
+    /**@type {HTMLDivElement} @readonly*/
+    progressText;
+    /**@type {HTMLDivElement} @readonly*/
+    progressBar;
+    /**@type {HTMLDivElement} @readonly*/
+    progressStatus;
+    /**@type {boolean} @readonly*/
+    valid;
+    constructor(/**@type {File}*/ file) {
+        if (!file.type.includes("video/")) return this.invalid("File is not a video.");
+        if (file.size > 500 * 1024 * 1024) return this.invalid("File is too big. (>500MB)");
+        this.valid = true;
+        this.formData = new FormData(); 
+        this.formData.append("file", file, file.name);
+        this.videoDiv = document.getElementById("videotemplate").content.cloneNode(true).querySelector("div.videobox");
+        this.progressText = this.videoDiv.querySelector("div.progress-text");
+        this.progressBar = this.videoDiv.querySelector("div.progress-bar");
+        this.progressStatus = this.videoDiv.querySelector("div.progress-status");
+        this.videoDiv.querySelector("span.title").innerText = file.name;
+        document.getElementById("uploadbar").insertAdjacentElement("afterend", this.videoDiv);
+    }
+    /**@private */
+    invalid(/**@type {string}*/ reason) {
+        this.valid = false;
+        showError(reason);
+    }
+    send() {
+        const myself = this;
+        this.xhr = new XMLHttpRequest();
+        this.xhr.open("POST", "/api/upload");
+        this.xhr.upload.onprogress = event => { myself.progress(Math.round(event.loaded / event.total * 100)) };
+        this.xhr.onload = event => { myself.uploadDone(event) };
+        this.xhr.send(this.formData);
+    }
+    /** @private */
+    progress(/**@type {Number}*/ progress) {
+        this.progressBar.style.width = progress + "%";
+        this.progressStatus.innerText = progress + "%";
+    }
+    /** @private */
+    async uploadDone(/**@type {ProgressEvent<EventTarget>}*/ event) {
+        console.log(event)
+        this.progressText.innerText = "Converting...";
+        try {
+            const response = JSON.parse(this.xhr.responseText);
+            if (response.error) {
+                return showError(response.error.message);
+            }
+            //this.videoDiv.querySelector("a.link").innerText = document.location.origin + "/v/" + response.video_id;
+            //this.videoDiv.querySelector("a.link").href = document.location.origin + "/v/" + response.video_id;
+            while (true) {
+                const res = await (await fetch(`/api/progress/${response.video_id}`)).json();
+                if (res.error) {
+                    return showError(res.error.message);
+                }
+                this.progress(Math.round(res.progress));
+
+                if (res.progress == 100) {
+                    this.done();
+                    break;
+                }
+                
+                await new Promise(resolve => setTimeout(resolve, 200));
+            }
+        }
+        catch {
+            return showError("Upload error");
+        }
+    }
+    /** @private */
+    done() {
+        this.videoDiv.querySelector("div.progress-wrp").style.display = "none";
+    }
+}
+
 
 (async () => {
     // wait until auth has gotten a user (i know this is a dumb way but it works)
@@ -34,5 +121,10 @@ function getRelativeDate(/** @type {number} */ timestamp) {
 
     if (user.type == "anonymous") {
         showAnonymousWarning();
+    }
+    document.getElementById("file").onchange = event => {
+        const upload = new Upload(event.target.files[0]);
+        if (!upload.valid) return;
+        upload.send();
     }
 })()
