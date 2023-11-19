0d744a26705ce415abe6b558831616b899495572
tpslr <topias.lyyra@gmail.com>
2023-11-20 00:44:03 +0200

    added redis for remembering file convert status

--- src/file_upload.py
+++ src/file_upload.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+import dataclasses
+import json
 from time import sleep
 import ffmpeg
 from flask import request
@@ -8,6 +10,7 @@ import subprocess
 import random
 import os
 from shutil import rmtree
+from redis import Redis
 
 ID_CHARACTERS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_'
 
@@ -18,6 +21,10 @@ if not VIDEO_FOLDER:
     raise ValueError("missing 'VIDEO_FOLDER' environment variable.")
 if not os.path.exists(VIDEO_FOLDER):
     os.mkdir(VIDEO_FOLDER)
+
+if not IS_DEV:
+    redis = Redis()
+    redis.select(2)
 
 db: SQLAlchemy = None
 
@@ -86,7 +93,12 @@ def transcode(owner: int, video_id: str, file_name: str, title: str):
 
     # initialize a TranscodeProgress object for this transcode
     video_duration = get_video_duration(input_video)
-    transcode_progresses[video_id] = TranscodeProgress(owner, title, video_duration, 0)
+    transcode_progress = TranscodeProgress(owner, title, video_duration, 0)
+    if IS_DEV:
+        transcode_progresses[video_id] = transcode_progresses
+    else:
+        save_transcode_progress_redis(video_id, transcode_progress)
+    
     ( # transcode video, reporting progress to http://localhost:5000/setprogress/{video_id}
         ffmpeg.input(input_video)
         .output(output_video, maxrate="1500k", progress=f"http://localhost:5000/api/setprogress/{video_id}", movflags="faststart")
@@ -107,9 +119,13 @@ def transcode(owner: int, video_id: str, file_name: str, title: str):
 def after_transcode(video_id: str):
     try:
         sql = text("INSERT INTO videos (id, owner, title, duration, private) VALUES (:id, :owner, :title, :duration, :private)")
-        duration = transcode_progresses[video_id].duration
-        owner = transcode_progresses[video_id].owner
-        title = transcode_progresses[video_id].title
+        if IS_DEV:
+            transcode_progress = transcode_progresses[video_id]
+        else:
+            transcode_progress = load_transcode_progress_redis(video_id)
+        duration = transcode_progress.duration
+        owner = transcode_progress.owner
+        title = transcode_progress.title
         db.session.execute(sql, { "id": video_id, "owner": owner, "title": title, "duration": duration, "private": False })
         db.session.commit()
     except:
@@ -122,25 +138,37 @@ def get_video_duration(file_path: str):
     return float(subprocess.check_output(["ffprobe", "-select_streams", "v:0", "-show_entries", "stream=duration", "-of", "csv=p=0", "-v", "error", file_path], stderr=subprocess.PIPE))
 
 def get_transcode_progress(video_id: str):
-    if video_id not in transcode_progresses:
+    transcode_progress = None
+    if IS_DEV:
+        transcode_progress = transcode_progresses[video_id]
+    else:
+        transcode_progress = load_transcode_progress_redis(video_id)
+    
+    if not transcode_progress:
         return create_error(f"No progress for video: \"{video_id}\"")
-    transcode_progress = transcode_progresses[video_id]
+
     # calculate transcode progress in %
     progress = transcode_progress.progress / transcode_progress.duration * 100
 
     # if the progress is 100%, remove it from the transcode_progresses dict, since it's not needed anymore
     # yes I know doing like this could cause a tiny memory leak if the client never requests the progress 
     if progress == 100:
-        transcode_progresses.pop(video_id)
+        if IS_DEV:
+            transcode_progresses.pop(video_id)
+        else:
+            redis.delete(video_id)
 
     return { "progress": progress }
 
 def set_transcode_progress(video_id: str):
     # this should never happen, but just in case
-    if video_id not in transcode_progresses:
-        cleanup_failed(video_id)
-        return "Error", 400
-    transcode_progress = transcode_progresses[video_id]
+    if not IS_DEV:
+        transcode_progress = load_transcode_progress_redis(video_id)
+    else:
+        if video_id not in transcode_progresses:
+            cleanup_failed(video_id)
+            return "Error", 400
+        transcode_progress = transcode_progresses[video_id]
 
     while True:
         # read one line at a time from the request stream
@@ -152,13 +180,31 @@ def set_transcode_progress(video_id: str):
         if line.startswith("out_time_us="):
             microseconds = float(line.split("=")[1])
             transcode_progress.progress = microseconds / 1000000
+            if not IS_DEV:
+                save_transcode_progress_redis(video_id, transcode_progress)
     
     after_transcode(video_id)
 
     # make sure the progress ends up at 100 (in case of rounding errors, etc.)
     transcode_progress.progress = transcode_progress.duration
+    if not IS_DEV:
+        save_transcode_progress_redis(video_id, transcode_progress)
     return "OK", 200
 
+
+def load_transcode_progress_redis(video_id: str):
+    result = redis.get(video_id)
+    if not result: return None
+    return TranscodeProgress(**json.loads(result))
+
+def save_transcode_progress_redis(video_id: str, transcode_progress: TranscodeProgress):
+    redis.mset({ video_id: json.dumps(transcode_progress, cls=DataclassEncoder)})
+
+class DataclassEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
 
 def create_error(message: str):
     return { "error": message }
